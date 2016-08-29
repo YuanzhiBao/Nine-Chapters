28ec2d5b712425e764cd8d81115120d7da7fc656
baoyu <baoyu@indiana.edu>
2016-08-29 12:40:07 -0400

    searchinsertposition

--- First Chapter/SearchInsertPosition/SearchInsertPosition.java
+++ First Chapter/SearchInsertPosition/SearchInsertPosition.java
@@ -1,4 +1,37 @@
 
 public class SearchInsertPosition {
-
+    /** 
+     * param A : an integer sorted array
+     * param target :  an integer to be inserted
+     * return : an integer
+     */
+    public int searchInsert(int[] A, int target) {
+        if (A == null || A.length == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = A.length - 1;
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (A[mid] == target) {
+                return mid;
+            }
+            if (A[mid] > target) {
+                end = mid;
+            }
+            if (A[mid] < target) {
+                start = mid;
+            }
+        }
+        if (A[start] >= target) {
+            return start;
+        }
+        if (A[end] == target) {
+            return end;
+        }
+        if (A[end] < target) {
+            return end + 1;
+        }
+        return start + 1;
+    }
 }
